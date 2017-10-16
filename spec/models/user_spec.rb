2658b323b6b89aedc2ad6ddf69b8e59f2234a8e6
Luiza Magalhaes <luizalmagalhaes@hotmail.com>
2017-10-16 16:43:14 -0200

    fixing errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,5 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:name)  }
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:phone) }
-  it { is_expected.to validate_presence_of(:cpf)  }
+  it { is_expected.to validate_presence_of(:cpf)   }
 end
